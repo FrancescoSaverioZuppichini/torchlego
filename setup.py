f959e15c7108e1ddf33657514ae769ec651e7d10
francesco <francesco.saverio.zuppichini@ch.pwc.com>
2020-01-12 18:29:10 +0100

    setup

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+
+     name='torchlego',
+     version='0.0.0',
+     packages=find_packages(),
+     author="Francesco Zuppichini",
+     author_email="francesco.zuppichini@gmail.com",
+     description="High level building blocks per Neural Networks with examples",
+     include_package_data=True,
+     url="https://github.com/FrancescoSaverioZuppichini/torchlego",
+ )
